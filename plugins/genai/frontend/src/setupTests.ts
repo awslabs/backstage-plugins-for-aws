9f50a36a69779f8dcc00d11680b2b0afe610fd36
Niall Thomson <thomson.niall@gmail.com>
2024-12-07 05:57:32 -0700

    Fix ui test

--- plugins/genai/frontend/src/setupTests.ts
+++ plugins/genai/frontend/src/setupTests.ts
@@ -12,3 +12,8 @@
  */
 
 import '@testing-library/jest-dom';
+import { TransformStream } from 'node:stream/web';
+
+Object.defineProperties(globalThis, {
+  TransformStream: { value: TransformStream },
+});
