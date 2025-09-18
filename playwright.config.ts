4842788ec293dd087923d913e6cbf83b135f085d
Niall Thomson <thomson.niall@gmail.com>
2025-09-18 17:26:28 -0600

    Fix syntax

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,22 +25,20 @@ export default defineConfig({
     timeout: 5_000,
   },
   // Run your local dev server before starting the tests
-  webServer: process.env.CI
-    : [
-        {
-          command: 'yarn start',
-          port: 3000,
-          reuseExistingServer: !process.env.CI,
-          timeout: 60_000,
-        },
-      ],
+  webServer: [
+    {
+      command: 'yarn start',
+      port: 3000,
+      reuseExistingServer: !process.env.CI,
+      timeout: 60_000,
+    },
+  ],
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   reporter: [['html', { open: 'never', outputFolder: 'e2e-test-report' }]],
   use: {
     actionTimeout: 0,
-    baseURL:
-      process.env.PLAYWRIGHT_URL ?? 'http://localhost:3000',
+    baseURL: process.env.PLAYWRIGHT_URL ?? 'http://localhost:3000',
     screenshot: 'only-on-failure',
     trace: 'on-first-retry',
   },
