ed6eeb3142a74a02bfaf1dda578bf856aec1fabf
secustor <sebastian@poxhofer.at>
2026-02-11 22:06:10 +0100

    chore: remove usages of @backstage/backend-common

--- plugins/core/node/src/locator/aws-config-locator.test.ts
+++ plugins/core/node/src/locator/aws-config-locator.test.ts
@@ -13,7 +13,7 @@
 
 import { ConfigReader } from '@backstage/config';
 import { mockClient } from 'aws-sdk-client-mock';
-import { getVoidLogger } from '@backstage/backend-common';
+import { mockServices } from '@backstage/backend-test-utils';
 import {
   DefaultAwsCredentialsManager,
   AwsCredentialProvider,
@@ -43,7 +43,7 @@ const getCredProviderMock = jest.spyOn(
 
 const awsConfigMock = mockClient(ConfigServiceClient);
 
-const logger = getVoidLogger();
+const logger = mockServices.logger.mock();
 
 describe('AWS Config locator', () => {
   beforeAll(async () => {});
